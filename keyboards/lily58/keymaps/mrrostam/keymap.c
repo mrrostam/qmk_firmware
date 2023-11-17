217465205d16b63dd5fcd9d7e42b06541c5af636
Pouya Rostam <pouya@picovoice.ai>
2023-11-17 08:45:44 -0800

    wip

--- keyboards/lily58/keymaps/mrrostam/keymap.c
+++ keyboards/lily58/keymaps/mrrostam/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,                               KC_Y,       KC_U,    KC_I,    KC_O,     KC_P,     KC_MINS,
       KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,                               KC_H,       KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,       KC_LBRC,      KC_RBRC,  KC_N,       KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
-                                    KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), MT(KC_LCTL, KC_SPC),       MT(KC_LALT, KC_ENT),   LT(_RAISE, KC_DEL), KC_BSPC, KC_DEL
+                                    KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), KC_SPC,       KC_ENT,   LT(_RAISE, KC_DEL), KC_BSPC, KC_DEL
       ),
 
     /* ,-----------------------------------------.                    ,-----------------------------------------.
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,                               KC_Y,       KC_U,    KC_I,    KC_O,     KC_P,     KC_MINS,
       KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,                               KC_H,       KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_TAB,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,       KC_LBRC,      KC_RBRC,  KC_N,       KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
-                                    KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), MT(KC_LCTL, KC_SPC),       MT(KC_LALT, KC_ENT),   LT(_RAISE, KC_DEL), KC_BSPC, KC_DEL
+                                    KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), KC_SPC,       KC_ENT,   LT(_RAISE, KC_DEL), KC_BSPC, KC_DEL
       ),
 
     /* ,-----------------------------------------.                    ,-----------------------------------------.
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_F,    KC_P,    KC_B,                               KC_J,       KC_L,    KC_U,    KC_Y,     KC_SCLN,  KC_MINS,
       KC_LCTL,  KC_A,     KC_R,     KC_S,    KC_T,    KC_G,                               KC_M,       KC_N,    KC_E,    KC_I,     KC_O,     KC_QUOT,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_D,    KC_V,       KC_LBRC,      KC_RBRC,  KC_K,       KC_H,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
-                                    KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), MT(KC_LCTL, KC_SPC),       MT(KC_LALT, KC_ENT),   LT(_RAISE, KC_DEL), KC_BSPC, KC_DEL
+                                    KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), KC_SPC,       KC_ENT,   LT(_RAISE, KC_DEL), KC_BSPC, KC_DEL
       ),
 
     /* ,-----------------------------------------.                    ,-----------------------------------------.
